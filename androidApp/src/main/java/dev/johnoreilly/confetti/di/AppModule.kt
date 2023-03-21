0334d15251627509b92cfd7091a07f270829b18b
John O'Reilly <oreillyj@gmail.com>
2023-03-21 22:16:52 +0000

    Theme settings screen

--- androidApp/src/main/java/dev/johnoreilly/confetti/di/AppModule.kt
+++ androidApp/src/main/java/dev/johnoreilly/confetti/di/AppModule.kt
@@ -15,9 +15,11 @@ import dev.johnoreilly.confetti.SpeakersViewModel
 import dev.johnoreilly.confetti.TokenProvider
 import dev.johnoreilly.confetti.account.AccountViewModel
 import dev.johnoreilly.confetti.account.Authentication
+import dev.johnoreilly.confetti.settings.SettingsViewModel
 import dev.johnoreilly.confetti.wear.WearSettingsSync
 import dev.johnoreilly.confetti.work.WorkManagerConferenceRefresh
 import org.koin.android.ext.koin.androidContext
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.androidx.viewmodel.dsl.viewModelOf
 import org.koin.core.module.dsl.bind
 import org.koin.core.module.dsl.singleOf
@@ -32,6 +34,8 @@ val appModule = module {
     viewModelOf(::SessionDetailsViewModel)
     viewModelOf(::SpeakerDetailsViewModel)
     viewModelOf(::AccountViewModel)
+    //viewModelOf(::SettingsViewModel)
+    viewModel { SettingsViewModel(get()) }
 
     single<ConferenceRefresh> { WorkManagerConferenceRefresh(get()) }
 
